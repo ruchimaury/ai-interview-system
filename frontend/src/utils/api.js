f7b845a8c304265e819716c41eed22a1c2f64a5f
Ruchi <ruchimaurya964@gmail.com>
2026-03-15 14:18:45 +0530

    api url fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL
+    ? `${process.env.REACT_APP_API_URL}/api`
+    : '/api'
+});
 
 API.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
